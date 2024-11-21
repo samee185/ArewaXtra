8b8348eacb5f957c32ac4065517996de3a669e74
sameeejsx <devsameee@gmail.com>
2024-11-21 23:31:05 +0100

    updates on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
   return (
     <>
-    <Navbar/>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,71 @@
-import React from 'react'
-import logo from "../assets/logo.png"
+import React from 'react';
+import logo from '../assets/logo.png';
+import { NavLink } from 'react-router-dom';
+import {
+  MagnifyingGlassIcon,
+  HeartIcon,
+  UserIcon,
+  ShoppingBagIcon,
+} from '@heroicons/react/24/outline';
+
 const Navbar = () => {
-    const navList = [
-        {
-            title:"home",
-            link:"/"
-        },
-        {
-            title:"shop",
-            link:"/shop"
-        },
-        {
-            title:"about us",
-            link:"/about"
-        },
-        {
-            title:"contact",
-            link:"/contact"
-        }
-    ]
+  const navList = [
+    {
+      title: 'home',
+      link: '/',
+    },
+    {
+      title: 'shop',
+      link: '/shop',
+    },
+    {
+      title: 'about us',
+      link: '/about',
+    },
+    {
+      title: 'contact',
+      link: '/contact',
+    },
+  ];
+
   return (
     <>
-    <div className="flex items center justify-between">
+      <div className="flex items-center justify-between p-4 bg-gray-50">
+        {/* Logo Section */}
         <div>
-            <img src={logo} alt="arewaxtra-logo" className='h-20' />
-        </div>
-        <div className='flex items-center gap-3 justify-evenly'>
-            {navList.map((index)=>{
-
-            })}
+          <img src={logo} alt="arewaxtra-logo" className="h-20" />
         </div>
 
-    </div>
+        {/* Navigation Links */}
+        <div className="hidden md:block flex items-center gap-5">
+          <ul className="flex gap-5">
+            {navList.map((item) => (
+              <li key={item.title}>
+                <NavLink
+                  to={item.link}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'text-yellow-500 font-bold capitalize' 
+                      : 'text-gray-700 hover:text-yellow-500 capitalize'
+                  }
+                >
+                  {item.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Icon Section */}
+        <div className="flex items-center gap-4">
+          <MagnifyingGlassIcon className="h-4 w-4 text-gray-700 hover:text-yellow-500 cursor-pointer" />
+          <UserIcon className="h-4 w-4 text-gray-700 hover:text-yellow-500 cursor-pointer" />
+          <HeartIcon className="h-4 w-4 text-gray-700 hover:text-yellow-500 cursor-pointer" />
+          <ShoppingBagIcon className="h-4 w-4 text-gray-700 hover:text-yellow-500 cursor-pointer" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
